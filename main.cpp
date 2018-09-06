e7df2e33b3bf647186d0836f347f884f2c1e1040
Your Name <hoangducanh11031995@gmail.com>
2018-09-06 04:39:54 -0400

    this is for newBranch2_2

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main()
 {
-	cout<<"This is for newBranch2"<<endl;
+	cout<<"This is for newBranch2_2"<<endl;
 	cout<<"This is for first commit"<<endl;
 	cout<<"This is for second commit"<<endl;
 	return 0;
